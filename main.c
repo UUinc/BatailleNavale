138cbbb98c8956054d1daec6227a444ae90d330d
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2021-04-07 22:38:59 +0100

    fixed error in the menu() function.

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <windows.h>
 
 //Global
+//to keep how much part of ship each player destroy and also to know the winner
+int Score[2];
 
 //Declaration
 int Menu(void);
 
 int main()
 {
-    printf("\\_____Bataille Navale_____/\n\n");
+    printf("_______ Bataille Navale _______\n\n");
     Menu();
     return 0;
 }
@@ -19,6 +22,7 @@ int Menu()
 
     printf("Menu :\n\n");
     printf("1.Play\n");
+    printf("2.How To Play\n");
     printf("0.Exit\n");
 
     do
@@ -26,7 +30,7 @@ int Menu()
         printf("\nChoice : ");
         fflush(stdin);
         error = !scanf("%d",&choice);
-
+        error = error || choice > 2 || choice < 0 ? 1 : 0;
         if(error)
             printf("choice incorrect!\ntry again\n");
     } while (error);
