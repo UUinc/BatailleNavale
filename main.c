a1aec8df9c19b65e46e51a9dc71fdc6e540d33c0
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2021-04-07 23:18:54 +0100

    DisplayMatrice() a dynamic function, still in work

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <windows.h>
+#include <conio.h>
 
 //Global
 //to keep how much part of ship each player destroy and also to know the winner
@@ -7,11 +7,19 @@ int Score[2];
 
 //Declaration
 int Menu(void);
+void DisplayMatrice(int dimension);
 
 int main()
 {
     printf("_______ Bataille Navale _______\n\n");
+    
     Menu();
+
+    DisplayMatrice(10);
+
+    printf("\nGood Bye!\n");
+    getch();
+
     return 0;
 }
 
@@ -36,4 +44,45 @@ int Menu()
     } while (error);
     
     return choice;
+}
+
+//Display Matrix 10 x 10
+void DisplayMatrice(int dimension)
+{
+    int i, j;
+
+    printf("\n");
+
+    for(i=0; i<dimension; i++)
+    {
+        for(j=0; j<dimension; j++)
+        {
+            if(j == 0 || j == dimension-1)
+            {
+                if(i!=0)
+                {
+                    if(j==0)
+                        printf("%c",64+i);
+                    else
+                        printf("|"); 
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            else if(i == 0 || i == dimension-1)
+            {
+                if(i==0)
+                    printf("%d",j);
+                else
+                    printf("_");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
