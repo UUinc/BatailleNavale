5034146b7472801c9f8ecbd317034712cd6e0ecd
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2021-04-30 00:02:39 +0000

    Title function added. DisplayGrid function fixed.

--- main.c
+++ main.c
@@ -12,21 +12,50 @@ int Score[2];
                                    {'B',' ',' ',' ',' ',' '}};
 
 //Declaration
+void Title();
+void TitleAscii();
+void BoatAscii();
 int Menu(void);
-void DisplayMatrice(void);
+void DisplayGrid(void);
 
 int main()
 {
     printf("_______ Bataille Navale _______\n\n");
-
+    
+    Title();
+    
     Menu();
 
-    DisplayMatrice();
+    DisplayGrid();
 
     printf("\nGood Bye!\n");
     getch();
 
     return 0;
+}
+
+void Title()
+{
+    TitleAscii();
+    BoatAscii();
+}
+void TitleAscii()
+{
+    printf("___  ____ ___ ____ _ _    _    ____    _  _ ____ _  _ ____ _    ____\n");
+    printf("|__] |__|  |  |__| | |    |    |___    |\\ | |__| |  | |__| |    |___\n");
+    printf("|__] |  |  |  |  | | |___ |___ |___    | \\| |  |  \\/  |  | |___ |___\n\n\n");
+}
+void BoatAscii()
+{
+    printf("              |    |    |\n");
+    printf("             )_)  )_)  )_)\n");
+    printf("            )___))___))___)\\\n");
+    printf("           )____)____)_____)\\\\\n");
+    printf("         _____|____|____|____\\\\\\__\n");
+    printf("---------\\                   /---------\n");
+    printf("  ^^^^^ ^^^^^^^^^^^^^^^^^^^^^\n");
+    printf("    ^^^^      ^^^^     ^^^    ^^\n");
+    printf("         ^^^^      ^^^\n\n");
 }
 
 //Display a Menu to let the user choose what to do
@@ -53,7 +82,7 @@ int Menu()
 }
 
 //Display Matrix 6 x 6
-void DisplayMatrice()
+void DisplayGrid()
 {
     int i, j, k, l;
     char grid[14][26]={{' ',' ',' ','1',' ',' ',' ','2',' ',' ',' ','3',' ',' ',' ','4',' ',' ',' ','5',' ',' ',' ','6',' ',' '},
@@ -71,16 +100,21 @@ void DisplayMatrice()
                        {'F','\xB3',   ' ',   ' ',   ' ','\xB3',   ' ',   ' ',   ' ','\xB3',   ' ',   ' ',   ' ','\xB3',   ' ',   ' ',   ' ','\xB3',   ' ',   ' ',   ' ','\xB3',   ' ',   ' ',   ' ','\xB3'},
                        {' ','\xC0','\xC4','\xC4','\xC4','\xC1','\xC4','\xC4','\xC4','\xC1','\xC4','\xC4','\xC4','\xC1','\xC4','\xC4','\xC4','\xC1','\xC4','\xC4','\xC4','\xC1','\xC4','\xC4','\xC4','\xD9'}};
     
+    //Change Grid Value
+    for(i=2,k=0; i<14; i+=2,k++)
+    {
+        for(j=3,l=0;j<26;j+=4,l++)
+        {
+            grid[i][j] = data[k][l];
+        }
+    }
+
+    //Display Grid
     for(i=0,k=0; i<14; i++)
     {
         for(j=0,l=0; j<26; j++)
         {
-            // if(i>1 && j>1 && i%2==0 && j%2==0) 
-            // {
-            //     printf("%c ",data[k][l]);
-            //     l++; k+= l>5?1:0;
-            // }
-            /*else*/ printf("%c",grid[i][j]);   
+            printf("%c",grid[i][j]);
         }
         printf("\n");
     }
