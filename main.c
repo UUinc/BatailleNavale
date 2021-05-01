3fc1120e40d7568f3dfea9c31dd48709817b2467
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2021-05-01 18:39:35 +0000

    set and get winner funct, Added HowToPlay funct

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
+
+//Structs
+typedef struct
+{
+    int x,y;
+}position;
+typedef struct
+{
+    char name[10];
+    //position of each box
+    position pos[3];
+}Navire;
+typedef struct
+{
+    char name[20];
+    int missiles;
+    int time;
+}Winner;
+
+//Function Declaration
+void Title();
+void TitleAscii();
+void BoatAscii();
+int Menu(void);
+void DisplayGrid(void);
+void SetWinner(void);
+void GetWinner(void);
+void HowToPlay(void);
 
 //Global
 //to keep how much part of ship each player destroy and also to know the winner
-int Score[2];
+Winner player2;
 /*deleted after */char data[6][6]={{'A','A','A',' ',' ',' '},
                                    {' ',' ',' ',' ',' ',' '},
                                    {' ',' ',' ','C',' ',' '},
@@ -11,23 +40,30 @@ int Score[2];
                                    {'B',' ',' ',' ',' ','C'},
                                    {'B',' ',' ',' ',' ',' '}};
 
-//Declaration
-void Title();
-void TitleAscii();
-void BoatAscii();
-int Menu(void);
-void DisplayGrid(void);
-
 int main()
 {
-    Title();
-    
-    Menu();
+    int result;
 
-    DisplayGrid();
+    do
+    {
+        Title();
+        result = Menu();
 
-    printf("\nGood Bye!\n");
-    getch();
+        switch(result)
+        {
+            case 1:
+                //Testing
+                DisplayGrid();
+                printf("Missiles and Time : ");scanf("%d%d",&player2.missiles,&player2.time);
+                SetWinner();
+                GetWinner();
+                break;
+            case 2: HowToPlay(); break;
+            case 0: printf("\nGood Bye!\n"); break;
+        }
+        getch();
+        system("cls");
+    } while (result);
 
     return 0;
 }
@@ -116,4 +152,34 @@ void DisplayGrid()
         }
         printf("\n");
     }
+}
+
+void SetWinner()
+{
+    printf("\n****** Winner ******\n\n");
+    printf("    Your Name : "); fflush(stdin); gets(player2.name);
+}
+
+void GetWinner()
+{
+    float score;
+
+    score = player2.time < player2.missiles ? (float)player2.missiles / (float)player2.time : (float)player2.time / (float)player2.missiles;
+    score *= 10;
+
+    printf("\n****** Winner ******\n\n");
+    printf("    Name  : %s\n",player2.name);
+    printf("    Score : %.0f\n",score);
+}
+
+void HowToPlay()
+{
+    printf("\nHow To Play : Bataille Navale\n\n");
+    printf("Au d\x82""but du jeu, chaque joueur place \x85 sa guise tous les bateaux sur sa grille de fa\x87on strat\x82gique. ");
+    printf("Le but \x82tant de compliquer au maximum la tache de son adversaire, c'est-\x85-dire d\x82truire tous vos navires.");
+    printf("\nLorem ipsum dolor sit amet, consectetur adipiscing elit, "
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
+    "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n");
 }
